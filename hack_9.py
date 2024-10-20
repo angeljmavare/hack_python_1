f50614b00d4834274b445774331128808e964d6d
AngelJMavare <angeljmavare@gmail.com>
2024-10-20 18:41:29 -0400

    feat: Hack_9.py

--- hack_9.py
+++ hack_9.py
@@ -3,6 +3,11 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 """
 
 def fn_hack_9():
-    result = [1,2,3]
-    #...
-    return result  
+    result = [1, 2, 3]
+    i = 1
+    while i <= len(result):
+        result.insert(i, "@")
+        i += 2
+    return result
+
+print("Resultado: ",fn_hack_9())
