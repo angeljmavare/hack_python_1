7847624f14ca421541dcee4ab7373441cee69d4e
AngelJMavare <angeljmavare@gmail.com>
2024-10-20 18:08:10 -0400

    feat: Hack_8.py

--- hack_8.py
+++ hack_8.py
@@ -4,5 +4,7 @@ list: [1,3,5,7,9] output => [3,5,7]
 
 def fn_hack_8():
     result = [1,3,5,7,9]
-    #...
-    return result  
+    resultado = result[1:-1]
+    return resultado
+
+print("Resultado es: ",fn_hack_8())
